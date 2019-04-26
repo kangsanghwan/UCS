ffe0a2b052755d9bdc138a7a1ca2de6599f3f28c
Oh <okm7327@naver.com>
2019-04-26 13:55:29 +0900

    업데이트?
    11

--- assets/js/pub/main.js
+++ assets/js/pub/main.js
@@ -1,6 +1,28 @@
-(function($){ 
+;(function($){ 
 
-	
 
+$(document).on('ready', function() {
+
+  var $vh = Math.floor(15.7 * $(window).height() / 100) + 'px';
+  console.log($vh)
+
+	$(".vertical-center-4").slick({
+		vertical: true,
+		centerMode: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerPadding: $vh,
+		adaptiveHeight: true,
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+
+	$('.vertical-center-4').on('afterChange', function(event, slick, currentSlide, nextSlide){
+		$('.view-fraction strong').text(currentSlide+1)
+		$('.menu-g ul li').eq(currentSlide).addClass('active').siblings('li').removeClass('active')
+	})
+});
 })(jQuery);
 
